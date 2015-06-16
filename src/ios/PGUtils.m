5df0845fc3032f553187a3de344ec881e7ee65ba
Ahmad <ahmad.moussawi@gmail.com>
2015-06-16 18:34:06 +0300

    update openStore/openApp

--- src/ios/PGUtils.m
+++ src/ios/PGUtils.m
@@ -6,9 +6,9 @@
  to you under the Apache License, Version 2.0 (the
  "License"); you may not use this file except in compliance
  with the License.  You may obtain a copy of the License at
-
+ 
  http://www.apache.org/licenses/LICENSE-2.0
-
+ 
  Unless required by applicable law or agreed to in writing,
  software distributed under the License is distributed on an
  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -22,20 +22,18 @@
 #import "PGUtils.h"
 #import "DeviceManager.h"
 
-#define APP_ID NSString [[NSBundle mainBundle] bundleIdentifier]
+#define APP_ID [[NSBundle mainBundle] bundleIdentifier]
 
-
-@interface PGUtils () {}
-@end
+NSString *URL_ITUNES = @"itms-apps://itunes.apple.com/us/app/%@";
 
 @implementation PGUtils
 
-- (void)getUniqueDeviceId:(CDVInvokedUrlCommand*)command
+- (void)getUniqueDeviceId:(CDVInvokedUrlCommand *)command
 {
     [DeviceManager setAppId:APP_ID];
     
     NSString *deviceID = [DeviceManager getDeviceIdFromKeychain];
-
+    
     if(!deviceID){
         deviceID = [DeviceManager generateDeviceId];
         // Store the key in the KeyChain
@@ -46,39 +44,63 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
-- (void) openStore:(CDVInvokedUrlCommand*) command {
-    
-    NSString* argument = [command.arguments objectAtIndex:0];
-    NSArray *arg = [argument componentsSeparatedByString:@"/"];
-    NSString *ID = [arg lastObject];
-
+- (void)openStore:(CDVInvokedUrlCommand *)command
+{
     CDVPluginResult* pluginResult = nil;
+    NSString* argument = [command.arguments objectAtIndex:0];
+    
+    if (![command.arguments count]) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:(@"No Arguments")];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        return;
+    }
+    
+    NSArray *arg = [argument componentsSeparatedByString:@"/"];
+    
+    if (![arg count]) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:(@"No ID")];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        return;
+    }
+
+    NSString *ID = [arg lastObject];
     UIApplication *myApp = [UIApplication sharedApplication];
-    NSString * appStoreUrl = [NSString stringWithFormat: @"itms-apps://itunes.apple.com/us/app/%@", ID];
+    NSString * appStoreUrl = [NSString stringWithFormat: URL_ITUNES, ID];
     [myApp openURL:[NSURL URLWithString:appStoreUrl]];
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:("MARKET")];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:(@"MARKET")];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
-- (void) openApp:(CDVInvokedUrlCommand*) command {
-    
+- (void) openApp:(CDVInvokedUrlCommand *)command
+{
     CDVPluginResult* pluginResult = nil;
     
+    if (![command.arguments count]) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:(@"No Arguments")];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        return;
+    }
+    
     NSString* argument = [command.arguments objectAtIndex:0];
-    NSArray *arg = [argument componentsSeparatedByString:@"/"];
-    NSString *scheme = [[arg firstObject] stringByAppendingString:@"://"];
-
+    NSArray *components = [argument componentsSeparatedByString:@"/"];
+    
+    if (![components count]) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:(@"No Scheme")];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        return;
+    }
+    
+    NSString *scheme = [[components firstObject] stringByAppendingString:@"://"];
+    
     UIApplication *myApp = [UIApplication sharedApplication];
     
     if ([myApp canOpenURL:[NSURL URLWithString:scheme]]) {
         [myApp openURL:[NSURL URLWithString:scheme]];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:("OK")];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:(@"OK")];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }
-    
-    else {
+    else{
         [self openStore:command];
     }
-    
 }
 @end
